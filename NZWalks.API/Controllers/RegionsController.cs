48165ce0c66b8df6cc08ff481bb6fa3edd5f118a
manoj.jaiswal <mjaiswal@landolakes.com>
2026-04-24 14:29:14 +0530

    added get in region

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
 
-namespace NZWalks.API.Controllers
+namespace NZWalks.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RegionsController(NZWalksDbContext dbContext) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RegionsController : ControllerBase
+    //Get All Regions
+    [HttpGet]
+    public IActionResult GetAll()
     {
-        
+        var regions = dbContext.Regions.ToList();
+        return Ok(regions);
+    }
+    
+    //Get Region by Id
+    [HttpGet]
+    [Route("{id:Guid}")]
+    public IActionResult GetById([FromRoute]Guid id)
+    {
+        //var result = dbContext.Regions.Find(id);
+        var result = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+        if (result == null)
+            return NotFound();
+        return Ok(result);
     }
 }
