b594fb99e3d91c8568c620972781c73b7546e00e
Manoj Jaiswal <manoj.jaiswal883@gmail.com>
2026-04-29 14:37:25 +0530

    added sorting

--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -6,7 +6,7 @@ namespace NZWalks.API.Repositories;
 public interface IWalkRepository
 {
     Task<Walk> CreateAsync(Walk walk);
-    Task<List<Walk>> GetAllWalkAsync(string? filterOn =null, string? filterQuery = null);
+    Task<List<Walk>> GetAllWalkAsync(string? filterOn =null, string? filterQuery = null, string? sortBy = null, bool isAscending = true);
     Task<Walk?> GetWalkByIdAsync(Guid id);
     Task<Walk?>UpdateAsync(Guid id, Walk walk);
     Task<Walk?> DeleteAsync(Guid id);
